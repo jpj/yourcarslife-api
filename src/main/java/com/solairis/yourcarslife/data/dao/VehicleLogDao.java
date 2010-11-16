7b209df059b61eb551c78e887858ee6cb357c8bc
Josh Paul Johnson <joshpauljohnson@gmail.com>
2010-11-16 07:39:34 -0500

    Correct hibernate syntax

--- src/main/java/com/solairis/yourcarslife/data/dao/VehicleLogDao.java
+++ src/main/java/com/solairis/yourcarslife/data/dao/VehicleLogDao.java
@@ -5,10 +5,15 @@
 
 package com.solairis.yourcarslife.data.dao;
 
+import com.solairis.yourcarslife.data.exception.VehicleLogDaoException;
+import com.solairis.yourcarslife.data.domain.VehicleLog;
+
 /**
  *
  * @author josh
  */
 public interface VehicleLogDao {
+
+	public VehicleLog getVehicleLog(long vehicleLogId) throws VehicleLogDaoException;
 
 }
---
+++ src/main/java/com/solairis/yourcarslife/data/domain/VehicleLog.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.solairis.yourcarslife.data.domain;
+
+import java.util.Date;
+
+/**
+ *
+ * @author josh
+ */
+public class VehicleLog {
+
+	private long vehicleLogId;
+	private Date created;
+	private Date modified;
+
+	public long getVehicleLogId() {
+		return vehicleLogId;
+	}
+
+	public void setVehicleLogId(long vehicleLogId) {
+		this.vehicleLogId = vehicleLogId;
+	}
+
+	public Date getCreated() {
+		return created;
+	}
+
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+
+	public Date getModified() {
+		return modified;
+	}
+
+	public void setModified(Date modified) {
+		this.modified = modified;
+	}
+}
