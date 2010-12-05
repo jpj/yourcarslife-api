e75727c2e3dd00ecbc940115338f6a54000654a3
Josh Paul Johnson <joshpauljohnson@gmail.com>
2010-12-05 12:21:13 -0500

    Changed from hql to criteria

--- src/main/java/com/solairis/yourcarslife/data/dao/impl/VehicleFuelLogDaoHibernate.java
+++ src/main/java/com/solairis/yourcarslife/data/dao/impl/VehicleFuelLogDaoHibernate.java
@@ -8,9 +8,12 @@ package com.solairis.yourcarslife.data.dao.impl;
 import com.solairis.yourcarslife.data.dao.VehicleFuelLogDao;
 import com.solairis.yourcarslife.data.exception.VehicleLogDaoException;
 import com.solairis.yourcarslife.data.domain.VehicleFuelLog;
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
 
 /**
  *
@@ -21,11 +24,19 @@ public class VehicleFuelLogDaoHibernate implements VehicleFuelLogDao {
 	private SessionFactory sessionFactory;
 
 	@Override
-	public VehicleFuelLog getVehicleFuelLog(long vehicleLogId) throws VehicleLogDaoException {
-		Session session = this.sessionFactory.getCurrentSession();
-		Query query = session.createQuery("from VehicleFuelLog log where log.vehicleFuelLogId=?");
-		query.setParameter(0, vehicleLogId);
-		return (VehicleFuelLog)query.uniqueResult();
+	public VehicleFuelLog getVehicleFuelLog(long vehicleFuelLogId) throws VehicleLogDaoException {
+
+		VehicleFuelLog vehicleFuelLog = null;
+
+		try {
+			Criteria criteria = this.sessionFactory.getCurrentSession().createCriteria(VehicleFuelLog.class);
+			criteria.add(Restrictions.eq("vehicleFuelLogId", vehicleFuelLogId));
+			vehicleFuelLog = (VehicleFuelLog)criteria.uniqueResult();
+		} catch (HibernateException e) {
+			throw new VehicleLogDaoException(e);
+		}
+
+		return vehicleFuelLog;
 	}
 
 	@Override
