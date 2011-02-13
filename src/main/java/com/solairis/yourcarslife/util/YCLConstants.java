400e0eabea12df4e04ae8902166f6a9383e0626e
Josh Paul Johnson <joshpauljohnson@gmail.com>
2011-02-12 19:32:14 -0500

    Added constants file

---
+++ src/main/java/com/solairis/yourcarslife/util/YCLConstants.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.solairis.yourcarslife.util;
+
+/**
+ *
+ * @author josh
+ */
+public class YCLConstants {
+
+	public static final String ROLE_USER = "ROLE_USER";
+
+}
